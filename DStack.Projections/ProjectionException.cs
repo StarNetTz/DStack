8489138c8b1d9c81e65425e4dc7ac5a3a27cb1c1
Zvjezdan Tomicevic <zeko77@gmail.com>
2022-01-28 12:46:58 +0100

    Minor cleanup

--- DStack.Projections/ProjectionException.cs
+++ DStack.Projections/ProjectionException.cs
@@ -13,8 +13,6 @@ namespace DStack.Projections
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (<see langword="Nothing" /> in Visual Basic) if no inner exception is specified.</param>
         public ProjectionException(string message, Exception innerException) : base(message, innerException)
-        {
-
-        }
+        {}
     }
 }
