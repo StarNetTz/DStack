af0d5e34e12f4d4be5c47006717382cc7efd1c9b
Zlatan Omerović <z.omerovic@globalsoft.ba>
2024-09-12 13:08:48 +0200

    feat(aggregate/interactor): idempotency methods abstraction and segregation

--- DStack.Aggregates.UnitTests/SampleDomain/PersonAggregateInteractor.cs
+++ DStack.Aggregates.UnitTests/SampleDomain/PersonAggregateInteractor.cs
@@ -5,13 +5,11 @@ namespace DStack.Aggregates;
 
 public interface IPersonAggregateInteractor : IInteractor { }
 
-public class PersonAggregateInteractor : Interactor, IPersonAggregateInteractor
+public class PersonAggregateInteractor : Interactor<PersonAggregate, PersonAggregateState>, IPersonAggregateInteractor
 {
-    readonly IAggregateRepository AggRepository;
-
     public PersonAggregateInteractor(IAggregateRepository aggRepository)
     {
-        AggRepository = aggRepository;
+        AggregateRepository = aggRepository;
     }
 
     public override async Task ExecuteAsync(object command)
@@ -27,28 +25,5 @@ public class PersonAggregateInteractor : Interactor, IPersonAggregateInteractor
     async Task When(RenamePerson c)
     {
         await IdempotentlyUpdateAgg(c.Id, agg => agg.Rename(c));
-    }
-
-    async Task IdempotentlyCreateAgg(string id, Action<PersonAggregate> usingThisMethod)
-    {
-        var agg = await AggRepository.GetAsync<PersonAggregate>(id).ConfigureAwait(false);
-        if (agg == null)
-            agg = new PersonAggregate(new PersonAggregateState());
-        var ov = agg.Version;
-        usingThisMethod(agg);
-        PublishedEvents = agg.PublishedEvents;
-        if (ov != agg.Version)
-            await AggRepository.StoreAsync(agg).ConfigureAwait(false);
-    }
-
-    async Task IdempotentlyUpdateAgg(string id, Action<PersonAggregate> usingThisMethod)
-    {
-        var agg = await AggRepository.GetAsync<PersonAggregate>(id).ConfigureAwait(false);
-        var ov = agg.Version;
-        usingThisMethod(agg);
-        PublishedEvents = agg.PublishedEvents;
-        if (ov == agg.Version)
-            return;
-        await AggRepository.StoreAsync(agg).ConfigureAwait(false);
     }
 }
---
+++ DStack.Aggregates/InteractorGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DStack.Aggregates;
+
+public abstract class InteractorGeneric
+{
+    protected List<object> PublishedEvents;
+
+    public abstract Task ExecuteAsync(object command);
+
+    public InteractorGeneric()
+    {
+        PublishedEvents = new List<object>();
+    }
+
+    public List<object> GetPublishedEvents()
+    {
+        return PublishedEvents;
+    }
+}
