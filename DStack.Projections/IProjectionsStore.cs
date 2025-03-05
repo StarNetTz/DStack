bd22029ca56d2d1d972847a44b98b072c85567fc
Zvjezdan Tomicevic <zeko77@gmail.com>
2025-03-05 14:10:43 +0100

    Added support to Load from store with objects as keys

--- DStack.Projections/IProjectionsStore.cs
+++ DStack.Projections/IProjectionsStore.cs
@@ -5,13 +5,15 @@ namespace DStack.Projections;
 
 public interface IProjectionsStore
 {
+    Task<T> LoadAsync<T>(string id) where T : class;
+    Task<Dictionary<string, T>> LoadAsync<T>(params string[] ids) where T : class;
+    Task<T> LoadAsync<T>(object id) where T : class;
+    Task<Dictionary<object, T>> LoadAsync<T>(params object[] ids) where T : class;
     Task StoreAsync(object doc);
     Task StoreInUnitOfWorkAsync(params object[] docs);
-    Task<T> LoadAsync<T>(string id) where T : class;
-    Task DeleteAsync(string id);
     Task StoreAsync<T>(T doc);
     Task StoreInUnitOfWorkAsync<T>(params T[] docs);
-    Task<Dictionary<string, T>> LoadAsync<T>(params string[] ids) where T : class;
+    Task DeleteAsync(string id);
     Task DeleteInUnitOfWorkAsync(params string[] ids);
 }
 
